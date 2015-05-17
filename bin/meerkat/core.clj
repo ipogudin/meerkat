37c7d7aca524db6d0df04299cda0bd3f1ad99e66
Ivan Pogudin <i.a.pogudin@gmail.com>
2015-05-17 20:03:54 +0600

    Removed temporary files.

--- bin/meerkat/core.clj
+++
@@ -1,7 +0,0 @@
-(ns meerkat.core
-  (:require [meerkat.httpservice.netty :as httpservice]))
-
-(defn run-http-service [timeout]
-  (let [service (httpservice/start {})]
-    (java.lang.Thread/sleep timeout)
-    (httpservice/stop service)))
